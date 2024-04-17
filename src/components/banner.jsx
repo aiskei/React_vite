451149926ebe13f8887139f0ce754e7c833586be
AishaKeita <aishacassel@gmail.com>
2024-04-17 10:33:37 +0200

    Added new commponents and a logo

---
+++ src/components/banner.jsx
@@ -0,0 +1,12 @@
+const Banner = () => {
+    return (
+        <header>
+            <div>
+                <img src="./src/assets/housingLogo.png" alt="logo" />
+            </div>
+            <div>Providing housing all over the world!</div>
+        </header>
+    )
+}
+
+export default Banner
---
+++ src/components/home.jsx
@@ -0,0 +1,8 @@
+import Banner from "./banner"
+
+const Home = () => {
+
+    return <Banner />
+}
+
+export default Home;
