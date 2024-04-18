d5d83343cad2100a6a1da1125a8b401424de8c93
AishaKeita <aishacassel@gmail.com>
2024-04-18 15:37:19 +0200

    Experimenting boostrap navigation bar

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,11 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import styles from './banner.module.css'
 const Banner = () => {
     return (
-        <header>
-            <div>
-                <img src="./src/assets/housingLogo.png" alt="logo" />
-            </div>
-            <div>Providing housing all over the world!</div>
-        </header>
+        <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home" className={styles.logo}>Housing Project</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link>Home</Nav.Link>
+              <Nav.Link href="#link">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     )
 }
 
