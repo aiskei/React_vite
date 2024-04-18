70e413afdbb2251478cb287b4cdc5e6db4fdd9d3
AishaKeita <aishacassel@gmail.com>
2024-04-18 15:38:46 +0200

    Modify main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './components/banner.jsx'
-import Home from './components/home.jsx'
-import UserList from './components/UserList.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
-        <Home />
-        <UserList />
     </React.StrictMode>
 )
